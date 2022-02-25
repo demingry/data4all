23399025e6c4f6e4156dec800d84bc5f91656689
Ubuntu <hzuna@ipv6-scanning.u5a5xr3odw4ullutfacsmnvv5e.hx.internal.cloudapp.net>
2022-02-25 08:58:33 +0000

    debug

--- main.go
+++ main.go
@@ -41,22 +41,21 @@ func main() {
 		}
 	}
 
+	selectors := make(map[string]string)
+	selectors[`title`] = `span#title`
 	nodesValue := nodes_instance.(IGetter).Getter()[1]
-	fmt.Println(nodesValue.([]string))
-	// selectors := make(map[string]string)
-	// selectors[`title`] = `span#title`
-	// nodesValue := nodes_instance.(IGetter).Getter()[1]
-	// for _, i := range nodesValue.([]string) {
-	// 	threads <- struct{}{}
-	// 	elements_instance := NewElements()
-	// 	ctx, cancel := InitDriver()
-	// 	go elements_instance.Execute(
-	// 		`https://dataverse.harvard.edu`+i,
-	// 		selectors,
-	// 		ctx,
-	// 		cancel,
-	// 	)
-	// }
+	for _, i := range nodesValue.([]string) {
+		threads <- struct{}{}
+		fmt.Println(i)
+		elements_instance := NewElements()
+		ctx, cancel := InitDriver()
+		go elements_instance.Execute(
+			`https://dataverse.harvard.edu`+i,
+			selectors,
+			ctx,
+			cancel,
+		)
+	}
 
 	// for {
 	// 	if len(threads) == 0 {
