006dd1696cbf2bc78c74ee1f9a33b72115fdf3fa
Ubuntu <hzuna@ipv6-scanning.u5a5xr3odw4ullutfacsmnvv5e.hx.internal.cloudapp.net>
2022-03-06 10:47:02 +0000

    debug

--- nodes.go
+++ nodes.go
@@ -85,6 +85,8 @@ func (n *Nodes) findNodes(url string,
 		url = fmt.Sprintf("%v", newurl)
 	}
 
+	fmt.Println("Scraping page: ", url)
+
 	if err := chromedp.Run(*ctx,
 		chromedp.Navigate(url),
 		chromedp.Nodes(selector, &nodes),
